684e4c78d00ad1843be3a83f02a28b0e0a61abe1
Virtue <dymurboi@gmail.com>
2025-05-28 08:53:46 +0800

    change

--- backend/CourtVision/src/main/java/cit/edu/capstone/CourtVision/service/TeamService.java
+++ backend/CourtVision/src/main/java/cit/edu/capstone/CourtVision/service/TeamService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
